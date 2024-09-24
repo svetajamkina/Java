d3740eb9b25c1141e435a4d8ebb3bbe98a9f1ce4
DESKTOP-QTUASIK\iljaj <svetlana.jamkina@gmail.com>
2024-09-24 22:14:19 +0300

    user service 2

---
+++ Datorium-API/src/main/java/com/datorium/Datorium/API/Services/UserService2.java
@@ -0,0 +1,8 @@
+package com.datorium.Datorium.API.Services;
+
+public class UserService2 {
+    public String randomData = "1234556";
+    public String getFullName(String name, String surname){
+        return name + " " + surname;
+    }
+}
--- Datorium-API/src/test/java/com/datorium/Datorium/API/DatoriumApiApplicationTests.java
+++ Datorium-API/src/test/java/com/datorium/Datorium/API/DatoriumApiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.datorium.Datorium.API;
 
+import com.datorium.Datorium.API.Services.UserService2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class DatoriumApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	// Hey, I need a method, that gives me my fullname
+	@Test
+	void WHEN_NameIsOskarsAndSurnameIsKlaumanis_THEN_Result_Oskars_Klaumanis() {
+		//Arrange -> prepare data and services
+		var userService = new UserService2();
+
+		//Act -> Do some action, usually call a method
+		var fullname = userService.getFullName("Oskars", "Klaumanis");
+
+		//Assert -> Test whether or not the result is correct
+		Assert.isTrue(fullname.equals("Oskars Klaumanis"), "Hey, the name should be with a space inbetween and should contain both name and surname");
 	}
 
 }
